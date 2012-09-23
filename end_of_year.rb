6e1003beca6fdadc3cd31182bff6616ae021e492
Emma Lawler <emily.lawler@colorado.edu>
2012-09-23 17:07:11 -0600

    time until the end of the year
    For both humans and dogs

---
+++ end_of_year.rb
@@ -0,0 +1,48 @@
+p today = time.now
+
+p end_of_the_year = Time.utc(2012,"dec",31)
+
+p end_of_the_year - today = 8472523.760915
+
+p seconds_until_the_end_of_the_year = 8472523.760915
+
+p seconds_until_the_end_of_the_year / 60 = minutes_until_the end_of_the_year
+
+p minutes_until_the_end_of_the_year = 141208.729348583
+
+p minutes_until_the_end_of_the_year / 60 = hours_until_the_end_of_the_year
+
+p hours_until_the_end_of_the_year = 2353.47882247638
+
+p hours_until_the_end_of_the_year / 24 = days_until_the_end_of_the_year
+
+p days_until_the_end_of_the_year = 98.0616176031825
+
+p days_until_the_end_of_the_year / 30 = months_until_the_end_of_the_year
+
+p months_until_the_end_of_the_year = 3.26872058677275
+
+p seconds_until_the_end_of_the_year * 7 = 59307666.326405
+
+p seconds_until_the_end_of_the_dog_year = 59307666.326405
+
+p minutes_until_the_end_of_the_year * 7 = 16474.3517573347
+
+p minutes_until_the_end_of_the_dog_year = 16474.3517573347
+
+p hours_until_the_end_of_the_year * 7 = 2353.47882247638
+
+p hours_until_the_end_of_the_dog_year = 2353.47882247638
+
+p days_until_the_end_of_the_year * 7 = 686.431323222277
+
+p days_until_the_end_of_the_dog_year = 686.431323222277
+
+p months_until_the_end_of_the_year * 7 = 22.8810441074092
+
+p months_until_the_end_of_the_dog_year = 22.8810441074092
+
+p time_until_end_of_year_for_humans = (3.2,98,2353,141208,8472523)
+
+p time_until_end_of_year_for_dogs = (22.8,686,2353,16474,59307666)
+
